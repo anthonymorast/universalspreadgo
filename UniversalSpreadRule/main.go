3f57d6afb01b33d938bea4080e1664d6472f09d1
Anthony Morast <anthonymorast33@hotmail.com>
2024-11-11 12:39:32 -0700

    Update main.go

--- UniversalSpreadRule/main.go
+++ UniversalSpreadRule/main.go
@@ -10,16 +10,6 @@ func arrayPasser(arr any) {
 }
 
 func main() {
-
-	// input := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
-	// output := make(chan int)
-	// go func() {
-	// 	for _, val := range input {
-	// 		output <- val
-	// 	}
-	// 	close(output)
-	// }()
-
 	var calculator universalspreadrule.ExcessMarginCalculator
 	request := universalspreadrule.NewMarginRequest(0.25, 0.3, 0.20)
 
@@ -27,7 +17,7 @@ func main() {
 	fmt.Println(int_arr)
 	arrayPasser(int_arr)
 
-	// iron connie
+	// iron conndor
 	request.AddOption("INTC", "INTC 241227C30000", 0.75, "241220", 1, universalspreadrule.Buy, universalspreadrule.Call, 30.)
 	request.AddOption("INTC", "INTC 241220C25000", 1.25, "241220", 1, universalspreadrule.Sell, universalspreadrule.Call, 25.)
 	request.AddOption("INTC", "INTC 241220P22000", 1.0, "241220", 1, universalspreadrule.Sell, universalspreadrule.Put, 22.)
@@ -38,8 +28,4 @@ func main() {
 	results := calculator.CalculateOrderMargin(request)
 
 	fmt.Println(results)
-
-	// for range input {
-	// 	fmt.Print(<-output, " ")
-	// }
 }
